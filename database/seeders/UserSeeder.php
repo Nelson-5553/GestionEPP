c3d2ff7f1a31812a47d6022f7e2a5995951f3fab
Nelson-5553 <andresnelson2004@hotmail.com>
2025-02-24 16:04:26 -0500

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john.doe@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password123'), // Puedes usar cualquier contraseña que desees
+            'current_team_id' => null,
+            'profile_photo_path' => null,
+        ]);
+
+        // Crear más usuarios de ejemplo si lo necesitas
+        User::create([
+            'name' => 'Jane doe',
+            'email' => 'jane.doe@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password123'),
+            'current_team_id' => null,
+            'profile_photo_path' => "profile-photos/mQvI2GJ77yxXE98zVvHU1fCJVGyDdmQhoUoAYJ7O.jpg",
+        ]);
+    }
+}
