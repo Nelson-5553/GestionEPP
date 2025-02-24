1dcc90c3250dcff998dacd6a20a397ec0ec1636f
Nelson-5553 <andresnelson2004@hotmail.com>
2025-02-23 19:03:02 -0500

    change dark mode script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+// modo oscuro del sistema
+    document.addEventListener('DOMContentLoaded', function () {
+        const html = document.documentElement;
+        const darkMode = localStorage.getItem('dark');
+
+        if (darkMode === 'true') {
+            html.classList.add('dark');
+        }
+
+        document.getElementById('toggleDarkMode').addEventListener('click', function () {
+            html.classList.toggle('dark');
+            localStorage.setItem('dark', html.classList.contains('dark'));
+        });
+    });
+
