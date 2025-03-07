0c172619793200843271cedaec90b8137fb63e02
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-07 15:46:44 -0500

    change text color menu sidebar when active

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'flex items-center rounded-sm gap-2 bg-black/10 px-2 py-1.5 text-sm font-medium text-[#5A6ACF] underline-offset-2 focus-visible:underline focus:outline-hidden dark:bg-white/10 dark:text-[#5A67BA]'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-[#5A6ACF] hover:text-[#5A6ACF] hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'flex items-center rounded-sm gap-2 bg-black/10 px-2 py-1.5 text-sm font-medium text-indigo-500 underline-offset-2 focus-visible:underline focus:outline-hidden dark:bg-white/10 dark:text-indigo-400'
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-700 hover:text-[#5A6ACF] hover:border-gray-300 focus:outline-none focus:text-[#5A6ACF] focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
