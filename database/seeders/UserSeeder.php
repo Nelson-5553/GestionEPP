3f7adb9baf6cabf149604ca1ff8505ff8cc4463c
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-10 12:53:00 -0500

    add factory for randon users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,26 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'John Doe',
-            'email' => 'john.doe@example.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password123'), // Puedes usar cualquier contraseña que desees
-            'current_team_id' => null,
-            'profile_photo_path' => null,
-            'card'=>'1014567890'
-        ])->assignRole('supervisor');
-
-        User::create([
-            'name' => 'Jane doe',
-            'email' => 'jane.doe@example.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password123'),
-            'current_team_id' => null,
-            'profile_photo_path' => null,
-            'card'=> '1023456789'
-        ])->assignRole('user');
-
+        // Usuario admin
         User::create([
             'name' => 'Super Admin',
             'email' => 'superadmin@example.com',
@@ -42,6 +23,32 @@ class UserSeeder extends Seeder
             'current_team_id' => null,
             'profile_photo_path' => null,
             'card'=> '1234567891'
-        ])->assignRole('admin');
-    }
+            ])->assignRole('admin');
+// Usuario Supervisor
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john.doe@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password123'), // Puedes usar cualquier contraseña que desees
+            'current_team_id' => null,
+            'profile_photo_path' => null,
+            'card'=>'1014567890'
+            ])->assignRole('supervisor');
+// Usuario user
+            User::create([
+                'name' => 'Jane doe',
+                'email' => 'jane.doe@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password123'),
+                'current_team_id' => null,
+                'profile_photo_path' => null,
+                'card'=> '1023456789'
+                ])->assignRole('user');
+
+                // Usuario user aleatorios
+                User::factory()->count(20)->create()->each(function ($user) {
+                 $user->assignRole('user');
+                });
+
+                }
 }
