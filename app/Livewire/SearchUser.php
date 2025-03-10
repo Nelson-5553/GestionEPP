158fc69f40813c913c7c737d4c2054035d70e795
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-10 12:24:37 -0500

    add filter component

--- app/Livewire/SearchUser.php
+++ app/Livewire/SearchUser.php
@@ -5,13 +5,27 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\User;
 use Livewire\WithPagination;
+use Livewire\Attributes\On;
 
 class SearchUser extends Component
 {
     use WithPagination;
+
+    public $search = '';
+
+    #[On('searchChangedUser')]
+    public function updateSearch($value)
+    {
+        $this->search = $value;
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $users = User::paginate(10);
+        $users = User::where('name', 'LIKE', '%' . $this->search . '%')
+        ->orWhere('card', 'LIKE', '%' . $this->search . '%')
+        ->orWhere('email', 'LIKE', '%' . $this->search . '%')
+        ->paginate(10);
         return view('livewire.search-user', compact('users'));
     }
 }
