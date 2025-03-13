d1f4de36f412212cbde203868f3a58429e1a0bc3
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-13 18:49:03 -0500

    change midleware in routes blocks

--- routes/web.php
+++ routes/web.php
@@ -23,66 +23,59 @@ Route::middleware([
     })->name('dashboard');
 });
 
-// Rurtas de sedes
 Route::middleware(['auth'])->group(function () {
+// Rurtas de sedes
     Route::get('sede', [SedeController::class, 'index'])->name('sede.index');
     Route::get('sede/edit/{sede}', [SedeController::class, 'edit'])->name('sede.edit');
     Route::get('sede/{sede}', [SedeController::class, 'show'])->name('sede.show');
     Route::post('sede', [SedeController::class, 'store'])->name('sede.store');
     Route::put('sede/{sede}', [SedeController::class, 'update'])->name('sede.update');
     Route::delete('sede/{sede}', [SedeController::class, 'destroy'])->name('sede.destroy');
-});
 
 // Rutas de area
-Route::middleware(['auth'])->group(function () {
+
     Route::get('area', [AreaController::class, 'index'])->name('area.index');
     Route::get('area/{area}',[AreaController::class, 'show'])->name('area.show');
     Route::get('area/edit/{area}', [AreaController::class, 'edit'])->name('area.edit');
     Route::post('area', [AreaController::class, 'store'])->name('area.store');
     Route::put('area/{area}', [AreaController::class, 'update'])->name('area.update');
     Route::delete('area/{area}',[AreaController::class, 'destroy'])->name('area.destroy');
-});
+
 // Rutas de Epp
-    Route::middleware(['auth'])->group(function () {
+
     Route::get('epp', [EppController::class, 'index'])->name('epp.index');
     Route::get('epp/{epp}', [EppController::class, 'show'])->name('epp.show');
     Route::get('epp/edit/{epp}', [EppController::class, 'edit'])->name('epp.edit');
     Route::post('epp', [EppController::class, 'store'])->name('epp.store');
     Route::put('epp/{epp}', [EppController::class, 'update'])->name('epp.update');
     Route::delete('epp/{epp}', [EppController::class, 'destroy'])->name('epp.destroy');
-});
 
-Route::middleware(['auth'])->group(function () {
 //Rutas de solicitud
+
     Route::get('solicitud', [SolicitudController::class, 'index'])->name('solicitud.index');
     Route::get('solicitud/create', [SolicitudController::class, 'create'])->name('solicitud.create');
     Route::get('solicitud/{solicitud}', [SolicitudController::class, 'show'])->name('solicitud.show');
     Route::patch('solicitud/{solicitud}', [SolicitudController::class, 'update'])->name('solicitud.update');
     Route::post('solicitud', [SolicitudController::class, 'store'])->name('solicitud.store');
-});
 
-
-
-Route::middleware(['auth'])->group(function () {
 //Rutas de Entrega
+
     Route::get('entrega', [EntregaController::class, 'index'])->name('entrega.index');
     Route::get('entrega/{entrega}', [EntregaController::class, 'show'])->name('entrega.show');
     Route::patch('entrega/{entrega}', [EntregaController::class, 'update'])->name('entrega.update');
 
-});
-
-Route::middleware(['auth'])->group(function () {
 //Rutas de Usuarios
+
     Route::get('user', [UserController::class, 'index'])->name('user.index');
     Route::get('user/{user}', [UserController::class, 'show'])->name('user.show');
     Route::get('user/edit/{user}', [UserController::class, 'edit'])->name('user.edit');
     Route::patch('user/edit/{user}', [UserController::class, 'update'])->name('user.update');
 
-});
 
-Route::middleware(['auth'])->group(function () {
-Route::get('role', [RoleController::class, 'index'])->name('role.index');
-Route::get('role/{role}', [RoleController::class, 'show'])->name('role.show');
-Route::get('role/create', [RoleController::class, 'create'])->name('role.create');
-Route::post('role', [RoleController::class, 'store'])->name('role.store');
+
+//Rutas de Roles
+    Route::get('role', [RoleController::class, 'index'])->name('role.index');
+    Route::get('role/{role}', [RoleController::class, 'show'])->name('role.show');
+    Route::get('role/create', [RoleController::class, 'create'])->name('role.create');
+    Route::post('role', [RoleController::class, 'store'])->name('role.store');
 });
