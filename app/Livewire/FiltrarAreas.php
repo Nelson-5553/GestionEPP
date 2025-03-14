3f735d094aee0e6831ec799e418464ee21c46107
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-13 20:18:10 -0500

    add optimize component FiltrarAreas

--- app/Livewire/FiltrarAreas.php
+++ app/Livewire/FiltrarAreas.php
@@ -24,7 +24,7 @@ class FiltrarAreas extends Component
     public function render()
     {
         return view('livewire.filtrar-areas', [
-            'sedes' => Sede::all(),
+            'sedes' => Sede::select('id', 'name')->get(),
         ]);
     }
 }
