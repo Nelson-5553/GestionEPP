bfa4608388d96adae9d213b1f37850ad2af52d07
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-16 15:25:53 -0500

    add reactive in succes alert

--- resources/views/components/success-menssage.blade.php
+++ resources/views/components/success-menssage.blade.php
@@ -1,6 +1,14 @@
 <div>
     @if (session('success'))
-        <div class="p-4 mt-4 text-sm text-green-700 bg-green-100 rounded-lg" role="alert">
+        <div
+            x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 3000)"
+            x-show="show"
+            x-transition.opacity.duration.1000ms
+            class="p-4 mt-4 text-sm text-green-700 bg-green-100 rounded-lg"
+            role="alert"
+            wire:ignore
+        >
             {{ session('success') }}
         </div>
     @endif
