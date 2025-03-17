969b8eb2ec57229bc9cb63a96c7b5f7972a2d307
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-17 12:25:59 -0500

    add card presentations in home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,39 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white  overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            <div class="overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="grid grid-cols-1 lg:grid-cols-3 w-full gap-4 mt-12">
+                    <div class="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md flex flex-col items-center text-center">
+                        <div class="w-12 h-12 bg-indigo-100 dark:bg-indigo-500 rounded-lg flex items-center justify-center mb-4 mx-auto">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+                            </svg>
+                        </div>
+                        <h3 class="text-xl font-semibold mb-2 text-gray-800 dark:text-white">Cumplimiento Normativo</h3>
+                        <p class="text-gray-600 dark:text-gray-300">Asegura el cumplimiento de normativas de seguridad y salud ocupacional.</p>
+                    </div>
+
+                    <div class="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md flex flex-col items-center text-center">
+                        <div class="w-12 h-12 bg-indigo-100 dark:bg-indigo-500 rounded-lg flex items-center justify-center mb-4 mx-auto">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                            </svg>
+                        </div>
+                        <h3 class="text-xl font-semibold mb-2 text-gray-800 dark:text-white">Análisis y Reportes</h3>
+                        <p class="text-gray-600 dark:text-gray-300">Obtén informes detallados para tomar decisiones basadas en datos.</p>
+                    </div>
+
+                    <div class="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md flex flex-col items-center text-center">
+                        <div class="w-12 h-12 bg-indigo-100 dark:bg-indigo-500 rounded-lg flex items-center justify-center mb-4 mx-auto">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </div>
+                        <h3 class="text-xl font-semibold mb-2 text-gray-800 dark:text-white">Control Eficiente</h3>
+                        <p class="text-gray-600 dark:text-gray-300">Gestiona inventarios y asignaciones de EPP con facilidad y precisión.</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
