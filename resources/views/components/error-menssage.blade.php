9d73a73c092522714b153fd0eb093b6440a637db
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-17 12:50:44 -0500

    add timer in error menssage

--- resources/views/components/error-menssage.blade.php
+++ resources/views/components/error-menssage.blade.php
@@ -1,11 +1,13 @@
 <div>
     @if ($errors->any())
-    <div class="p-4 mt-4 text-sm text-red-700 bg-red-100 rounded-lg" role="alert">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition.opacity.duration.2000ms
+            class="p-4 mt-4 text-sm text-red-700 bg-red-100 rounded-lg" role="alert" wire:ignore>
+
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 </div>
