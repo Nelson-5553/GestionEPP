e27790fa4a61cf30f8d9cab363b6e09fff6fc9fd
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-25 12:29:19 -0500

    remove vanilla styles

--- resources/views/role/RoleCreate.blade.php
+++ resources/views/role/RoleCreate.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto py-12 px-6 lg:px-8">
         <p class="text-xl font-bold text-gray-900 dark:text-gray-200 md:-ml-5">
-            Gestión Salud Roles
+            Gestión Salud Roles y Permisos
         </p>
 
          {{-- mensaje de error --}}
