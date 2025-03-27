b8ce390a64cf24ac7dea14191a92103c37cb23d8
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-26 23:53:19 -0500

    change sede name in Sede seeder

--- database/seeders/SedeSeeder.php
+++ database/seeders/SedeSeeder.php
@@ -27,9 +27,9 @@ class SedeSeeder extends Seeder
             'image' => 'mauxiliadora.png',
         ]);
         Sede::create([
-            'name' => 'Amberes',
-            'direction' => 'Cra. 38 #29-18, Alcibia, Cartagena de Indias',
-            'description' => 'Gestion salud Amberes',
+            'name' => 'Santa Marta',
+            'direction' => '6RR7+FM, Av. El Libertador #24 - 107, Comuna 3, Santa Marta, Magdalena',
+            'description' => 'Gestion salud Santa Marta',
             'image' => 'Amberes.jpeg',
         ]);
     }
