bbed60bf6ea423486b5470f8bdd552854b321b21
Nelson-5553 <andresnelson2004@hotmail.com>
2025-03-28 23:36:11 -0500

    add skeleton in userindex table

--- resources/views/livewire/search-user.blade.php
+++ resources/views/livewire/search-user.blade.php
@@ -2,7 +2,42 @@
 {{ $users->links() }}
 <div class="flex flex-row mt-6  rounded-lg">
     <div class="overflow-hidden w-full overflow-x-auto rounded-xl border border-neutral-300 dark:border-neutral-700">
-        <table class="w-full text-left text-sm text-neutral-600 dark:text-neutral-300">
+        <table wire:loading.table class="w-full text-left text-sm text-neutral-600 dark:text-neutral-300 animate-pulse">
+            <thead class="border-b border-[#5A6ACF] bg-[#5A6ACF] text-sm text-white">
+                <tr>
+                    <th scope="col" class="p-4">ID</th>
+                    <th scope="col" class="p-4">Nombre</th>
+                    <th scope="col" class="p-4">Correo</th>
+                    <th scope="col" class="p-4">Role</th>
+                    <th scope="col" class="p-4">Revisar</th>
+                </tr>
+            </thead>
+            <tbody class="divide-y divide-neutral-300 dark:divide-neutral-700">
+                @for ($i = 0; $i < 5; $i++)
+                    <tr class="animate-pulse">
+                        <td class="p-4">
+                            <div class="h-4 w-4 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                        </td>
+                        <td class="p-4">
+                            <div class="h-4 w-20 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                        </td>
+
+                        <td class="p-4">
+                            <div class="h-4 w-64 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                        </td>
+
+                        <td class="p-4">
+                            <div class="h-4 w-16 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                        </td>
+
+                        <td class="p-4">
+                            <div class="h-4 w-20 bg-gray-300 dark:bg-gray-700 rounded"></div>
+                        </td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
+        <table wire:loading.remove class="w-full text-left text-sm text-neutral-600 dark:text-neutral-300">
             <thead class="border-b border-[#5A6ACF] bg-[#5A6ACF] text-sm text-white">
                 <tr>
                     <th scope="col" class="p-4">ID</th>
