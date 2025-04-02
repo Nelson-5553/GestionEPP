507655975ae3cf4ae5c8cbb82fc98ba330cdc54d
Nelson-5553 <andresnelson2004@hotmail.com>
2025-04-02 18:12:40 -0500

    fix error in cancel-modal

--- resources/views/components/cancel-modal.blade.php
+++ resources/views/components/cancel-modal.blade.php
@@ -17,7 +17,7 @@
                             <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM8.28 7.22a.75.75 0 0 0-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L10 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L11.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L10 8.94 8.28 7.22Z" clip-rule="evenodd" />
                         </svg>
                     </div>
-                    <button x-on:click="dangerModalIsOpen = false" aria-label="close modal">
+                    <button x-on:click="dangerModalIsOpen = false" type="button" aria-label="close modal">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" stroke="currentColor" fill="none" stroke-width="1.4" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
                         </svg>
