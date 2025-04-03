0bdd095fd1e5610508a3540b8b1d553a93d4ab2c
Nelson-5553 <andresnelson2004@hotmail.com>
2025-04-03 12:19:45 -0500

    change grid configuration

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             Bienvenido al Sistema de Gestión EPP
         </h1>
 
-        <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <section class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-4">
             @php
                 $fullWidth = Gate::allows('ver dashboard') ? 'col-span-3' : 'col-span-2';
             @endphp
